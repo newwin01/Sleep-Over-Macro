009880978e99a27964d5d507e1e2e96cf8ac6989
newwin01 <newwin0198@handong.ac.kr>
2023-02-28 00:30:10 +0900

    Organize

--- macro.py
+++ macro.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 hisnet_id = input("Enter hisnet id: ")
 hisnet_pw = input("Enter hisnet pw: ")
 
+#check the info
 print(hisnet_id)
 print(hisnet_pw)
 
@@ -18,23 +19,15 @@ driver = webdriver.Chrome(options=options)
 
 URL = 'https://hisnet.handong.edu/'
 
+#access to the homepage
 driver.get(URL)
 
-# open URL
-# URL = 'https://rc.handong.edu/rc/mypage/domExeat/index.do?menu_idx=70'
 time.sleep(5)
 
 # enter user data - login in to hisnet
-
-# elem = driver.find_element(By.CSS_SELECTOR, "id")
-# elem.click()
-
 frame = driver.find_element(by=By.NAME, value="MainFrame")
 driver.switch_to.frame(frame)
-
 time.sleep(5)
-
-# driver.switch_to
 
 log_ID = driver.find_element(by = By.NAME, value = "id")
 log_ID.send_keys(hisnet_id)
@@ -42,20 +35,7 @@ log_ID.send_keys(hisnet_id)
 log_PW = driver.find_element(by= By.NAME, value= "password")
 log_PW.send_keys(hisnet_pw)
 
+#login to hisnet by clicking
 WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//input[@type="image"][@src="/2012_images/intro/btn_login.gif"]'))).click()
-
 time.sleep(5)
 
-# driver.find_element(By.NAME("password")).send_keys(str(hisnet_pw))
-
-# WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//input[@type="image"][@src="/2012_images/intro/btn_login.gif"]'))).click()
-
-
-# driver.implicitly_wait(time_to_wait=20)
-
-
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# browser = webdriver.Chrome(options=options)
-
-# browser.get()
