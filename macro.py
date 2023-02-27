f76c5f707895a0040519c91a9eadcefa349e4949
newwin01 <newwin0198@handong.ac.kr>
2023-02-28 00:22:06 +0900

    Login_to_hisnet_completed

--- macro.py
+++ macro.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 # input = hisnet id & password
 hisnet_id = input("Enter hisnet id: ")
 hisnet_pw = input("Enter hisnet pw: ")
+
+print(hisnet_id)
+print(hisnet_pw)
 
 # use chrome
 options = webdriver.ChromeOptions()
@@ -18,22 +22,36 @@ driver.get(URL)
 
 # open URL
 # URL = 'https://rc.handong.edu/rc/mypage/domExeat/index.do?menu_idx=70'
-
-
-driver.implicitly_wait(time_to_wait=5)
+time.sleep(5)
 
 # enter user data - login in to hisnet
 
-id = driver.find_element(By.NAME, "id")
-id.send_keys(hisnet_id)
+# elem = driver.find_element(By.CSS_SELECTOR, "id")
+# elem.click()
 
+frame = driver.find_element(by=By.NAME, value="MainFrame")
+driver.switch_to.frame(frame)
 
-driver.find_element(By.NAME("password")).send_keys(str(hisnet_pw))
+time.sleep(5)
+
+# driver.switch_to
+
+log_ID = driver.find_element(by = By.NAME, value = "id")
+log_ID.send_keys(hisnet_id)
+
+log_PW = driver.find_element(by= By.NAME, value= "password")
+log_PW.send_keys(hisnet_pw)
+
+WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//input[@type="image"][@src="/2012_images/intro/btn_login.gif"]'))).click()
+
+time.sleep(5)
+
+# driver.find_element(By.NAME("password")).send_keys(str(hisnet_pw))
 
 # WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,'//input[@type="image"][@src="/2012_images/intro/btn_login.gif"]'))).click()
 
 
-driver.implicitly_wait(time_to_wait=20)
+# driver.implicitly_wait(time_to_wait=20)
 
 
 # options = webdriver.ChromeOptions()
